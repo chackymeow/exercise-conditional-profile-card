a48e65158e6b1f615516043f480527227a6e5fe4
chackymeow <fabichasola@hotmail.com>
2021-02-11 15:17:59 +0000

    avance final

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import "../style/index.scss";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
+/** 
+ *  EDIT ONLY  INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
     {
